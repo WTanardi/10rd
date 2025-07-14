2e90911c431a256b5caaa4dd1d5ffeb6e514b929
WTanardi <william.tanardi@gmail.com>
2025-07-14 19:55:52 +0700

    updated about page

--- app/(app)/about/page.tsx
+++ app/(app)/about/page.tsx
@@ -1,45 +1,97 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface FAQ {
+	question: string,
+	answer: string
+}
+
+const faq: FAQ[] = [
+	{
+		question: "Why is the website called 10rd?",
+		answer: "It's a wordplay of my surname 'Tanardi', 10rd is read (ˈtɛnˈɑrˈdi)"
+	}
+]
+
 export default function Home() {
 	return (
 		<div>
-			<div className="container relative bg-gray-900 mx-auto">
-				<section className="py-8 text-center px-8">
-					<h1 className="text-7xl lg:text-8xl font-sans font-bold pb-4">
-						William Tanardi
-					</h1>
-					<h2 className="pb-8 font-sans italic">
-						Bachelor of Information Systems at Universitas Airlangga | Software Engineer
-					</h2>
-					<p className="font-sans text-justify">
-
-						Hello, my name is William Tanardi, I am a Software Engineer passionate about enhancing and optimizing web experiences.
-
-						<br />
-						<br />
-
-						My journey into Software development began ever since I was little, it was my dad who inspired and introduced me to tinker with computers.
-						Seeing my dad work on his computer fascinated me and from then on forward, I started to teach myself to do things on my computer along with my brothers.
-						Over the past 5 years, I&apos;ve honed my skills in UI/UX design, Front-end web development, and Machine learning, working with various clients to create optimized experiences.
-
-						<br />
-						<br />
-
-						What sets me apart is my ability to adapt quickly and do efficient work.
-						I believe in providing accessibility for all and doing so in the most efficient way possible.
-						Allowing everyone to enjoy these tailored experiences while providing great value.
-
-						<br />
-						<br />
-
-						Outside of Software development, you&apos;ll often find me exploring one of my many hobbies, such as playing music, speedcubing, watch collecting, playing games, etc.
-						I&apos;m constantly exploring ways to improve myself and test my limits.
-
-						<br />
-						<br />
-
-						If you&apos;re looking to enhance yourself or your business, or just want to connect, feel free to reach out to one of my social media accounts.
-					</p>
+			<div className="container relative bg-gray-900 mx-auto font-sans max-w-sm md:max-w-xl lg:max-w-3xl">
+				<section className="p-8 flex flex-col justify-center items-center gap-4">
+					<Image src={"/williamtanardi.jpg"} height={250} width={250} alt="a photo of william tanardi" />
+					<p>Jack of all trades</p>
 				</section>
-			</div>
-		</div>
+				<section className="p-8 flex flex-col gap-4">
+					<div className="text-left">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							Who
+						</h2>
+						<div>
+							My name is William Tanardi, but most people call me either Willy or Billy
+						</div>
+					</div>
+					<div className="text-right">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							Where
+						</h2>
+						<p>
+							I was born and raised in Indonesia
+						</p>
+					</div>
+					<div className="text-left">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							What
+						</h2>
+						<p>
+							This is a personal website I've built with a deck of cards as the concept.
+						</p>
+					</div>
+					<div className="text-right">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							Why
+						</h2>
+						<p>
+							Because I wanna share and showcase many parts of my life.
+						</p>
+					</div>
+					<div className="text-left">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							When
+						</h2>
+						<p>
+							This website will be in continuous development, as I grow, this website grows as well.
+						</p>
+					</div>
+					<div className="text-right">
+						<h2 className="text-xl font-bold lg:text-3xl">
+							How
+						</h2>
+						<p>
+							Currently, this website is built using NextJS, you can see the source code right <Link href={"https://github.com/WTanardi/10rd"} className="text-cyan-300 underline">here</Link>
+						</p>
+					</div>
+				</section>
+				<section className="p-8 flex flex-col justify-center items-center gap-4 text-center">
+					<h2 className="text-3xl font-bold">
+						FAQ:
+					</h2>
+					<ul>
+						{
+							faq.map((e, i) => (
+								<div key={i}>
+									<li className="font-semibold">
+										{e.question}
+									</li>
+									<li>
+										{e.answer}
+									</li>
+								</div>
+							))
+						}
+						<li></li>
+					</ul>
+				</section>
+			</div >
+		</div >
 	);
 }
